bf75861511960aea2ae3bc6747672d8b864b595b
Madu Jang <madu.jang93@gmail.com>
2023-06-12 05:56:12 -0700

    empty sentence fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     s_len = len(sentence)
     if s_len == 0:
-        return None
-
-    s_first = sentence[0]
+        s_first = None
+    else:
+        s_first = sentence[0]
     return s_len, s_first
