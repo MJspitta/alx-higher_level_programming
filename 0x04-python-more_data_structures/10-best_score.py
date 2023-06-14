80e1160a8f89e4075e2c6712a1abe973a0374bd2
Madu Jang <madu.jang93@gmail.com>
2023-06-14 13:17:03 -0700

    student with best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    best = 0
+    if a_dictionary is None:
+        return None
+    for score in a_dictionary.values():
+        if score > best:
+            best = score
+    for key, val in a_dictionary.items():
+        if best == val:
+            return a_dictionary[key]
