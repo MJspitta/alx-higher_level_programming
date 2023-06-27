715b1f628cc16785f69a0b6a37f02f6fcdb1bcdf
Madu Jang <madu.jang93@gmail.com>
2023-06-27 10:54:04 -0700

    square class initialized

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Defines a square """
+
+
+class Square:
+    """ Class for a square """
+
+    def __init__(self, size):
+        """ Initialize the class Square
+        Args:
+            size - size of the square
+        """
+
+        self.__size = size
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -1,3 +1,5 @@
 0x06. Python - Classes and Objects
 
 0. My first square
+
+1. Square with size
