b6f1bc481df96501de656a7c79f9756b3c21af1e
Madu Jang <madu.jang93@gmail.com>
2023-07-10 13:03:23 -0700

    fixing

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,4 +9,5 @@ def inherits_from(obj, a_class):
     or returns False """
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
-    return False
+    else:
+        return False
