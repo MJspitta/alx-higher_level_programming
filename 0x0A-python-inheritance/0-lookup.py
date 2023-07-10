85f7888edcbb2cc66acc2421d60001d57c0d1ae5
Madu Jang <madu.jang93@gmail.com>
2023-07-10 09:11:07 -0700

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" list of available attributes and methods of an object """
+
+
+def lookup(obj):
+    """ function that returns list of available attributes
+    and methods of an object """
+    return dir(obj)
