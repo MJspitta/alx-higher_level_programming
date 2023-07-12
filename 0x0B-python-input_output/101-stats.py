46b258af159d5fae664be69d99ebbb59f679137b
Madu Jang <madu.jang93@gmail.com>
2023-07-12 05:57:32 -0700

    major fix

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,39 +3,48 @@
 
 
 import sys
-from collections import defaultdict
 
+
+def print_stat(size, stat_codes):
+    """ print accumulated metrics """
+    print("File size:", size)
+    for code in sorted(stat_codes):
+        print("{}: {}".format(code, stat_codes[code]))
 
 def comp_metrics():
     """ func that reads stdin line by line and computes metrics """
     size = 0
-    stat_codes = defaultdict(int)
+    stat_codes = {}
     counter = 0
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
 
     try:
         for line in sys.stdin:
-            counter += 1
-            parts = line.strip().split()
-            if len(parts) >= 7:
-                stat_code = parts[-2]
-                fsize = parts[-1]
-                try:
-                    size += int(fsize)
-                except ValueError:
-                    pass
+            if counter == 10:
+                print_stat(size, stat_codes)
+                counter = 1
+            else:
+                counter += 1
 
-                if stat_code.isdigit():
-                    stat_codes[stat_code] += 1
+            line = line.split()
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
 
-            if counter % 10 == 0:
-                print("File size:", size)
-                for code in sorted(stat_codes.keys()):
-                    print("{}: {}".format(code, stat_codes[code]))
+            try:
+                if line[-2] in valid_codes:
+                    if stat_codes.get(line[-2], -1) == -1:
+                        stat_codes[line[-2]] = 1
+                    else:
+                        stat_codes[line[-2]] += 1
+            except IndexError:
+                pass
+        print_stat(size, stat_codes)
 
     except KeyboardInterrupt:
-        print("Total file size:", size)
-        for code in sorted(stat_codes.keys()):
-            print("{}: {}".format(code, stat_codes[code]))
+        print_stat(size, stat_codes)
+        raise
 
 
 if __name__ == '__main__':
