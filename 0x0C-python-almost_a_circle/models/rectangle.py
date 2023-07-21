7d09de4fe16fe46350f6ab8c0df494818149ff63
Madu Jang <madu.jang93@gmail.com>
2023-07-21 05:55:02 -0700

    fixes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -90,15 +90,13 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
                 self.id, self.__x, self.__y,
                 self.__width, self.__height)
-
-    def update(self, *args):
-        """ update rectangle attributes """
+    def update(self, *args, **kwargs):
+        """ update rectagle attributes """
         attrib = ["id", "width", "height", "x", "y"]
         if args:
-            for i, a in enumerate(args):
+            for i, arg in enumerate(args):
                 if i < len(attrib):
-                    setattr(self, attrib[i], a)
-        else:
-            for key, val in kwargs.items():
-                if key in attrib:
-                    setattr(self, key, val)
+                    setattr(self, attrib[i], arg)
+        for key, val in kwargs.items():
+            if key in attrib:
+                setattr(self, key, val)
