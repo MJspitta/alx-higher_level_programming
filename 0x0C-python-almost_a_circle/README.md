fac14df4b68a0297c17f275d7eae411729627565
Madu Jang <madu.jang93@gmail.com>
2023-07-21 04:53:15 -0700

    area and display

--- 0x0C-python-almost_a_circle/README.md
+++ 0x0C-python-almost_a_circle/README.md
@@ -3,3 +3,11 @@
 0. If it's not tested it doesn't work
 
 1. Base class
+
+2. First Rectangle
+
+3. Validate attributes
+
+4. Area first
+
+5. Display #0
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -63,3 +63,10 @@ class Rectangle(Base):
         if val < 0:
             raise ValueError("y must be >= 0")
         self.__y = val
+
+    def area(self):
+        return self.__width * self.__height
+
+    def display(self):
+        for i in range(self.__height):
+            print("#" * self.__width)
