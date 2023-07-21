bfb991c7ed1b34770ab955396fd78a84081f7c87
Madu Jang <madu.jang93@gmail.com>
2023-07-21 05:13:08 -0700

    display 1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -78,8 +78,10 @@ class Rectangle(Base):
 
     def display(self):
         """ display rectangle """
+        for i in range(self.__y):
+            print()
         for i in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """ print format
