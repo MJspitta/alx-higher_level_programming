77d353ce225d11220a7021179a2d8efba2dc867b
Madu Jang <madu.jang93@gmail.com>
2023-09-12 18:58:52 -0700

    minor fix

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  const revList = list.reverse();
+  const revList = [];
+  let first = 0;
+  const last = list.length - 1;
+  for (first; first < list.length; first++) {
+    revList.push(list[last - first]);
+  }
   return revList;
 };
