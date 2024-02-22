9d110be5f808217c3b55f9ea9d7691ce900a9cdc
Madu Jang <madu.jang93@gmail.com>
2024-02-21 16:14:32 -0800

    status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], function (err, resp) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + resp.statusCode);
+  }
+});
